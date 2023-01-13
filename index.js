a2d8709fe40691f98b4d38c93a50e46421d83eb9
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-01-13 02:25:52 -0500

    commit tree 9a8cd90d3dfdf713bae6d4a74811baf18964dfc3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+var countdown = function countdown (i) {
+    while ( i >= 0) {
+        return '' + i+ countdown(i -= 1);
+    }
+    return '';
+
+};
+console.log(countdown(5));
